208fa6845dfd8e6080605f033d3d8fea5c6fd9e1
Hu Ziming <hzmangel@gmail.com>
2016-07-30 16:09:18 +0800

    Update golang solution

--- 1-Two-Sum/solution.go
+++ 1-Two-Sum/solution.go
@@ -1,3 +1,13 @@
 func twoSum(nums []int, target int) []int {
-    
+	rslt := []int{0, 0}
+	for idx_1, v_1 := range nums {
+		for idx_2, v_2 := range nums[idx_1+1:] {
+			if v_1+v_2 == target {
+				rslt[0] = idx_1
+				rslt[1] = idx_2 + idx_1 + 1
+				break
+			}
+		}
+	}
+	return rslt
 }

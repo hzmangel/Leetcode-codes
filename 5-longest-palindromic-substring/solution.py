b2de902b563b044d72461633c8620c9419bb6cb8
Hu Ziming <hzmangel@gmail.com>
2016-10-20 01:16:01 +0800

    Accepted, 1615 ms, 11.78%, :disappointed_relieved:
    https://leetcode.com/submissions/detail/79167764/

---
+++ 5-longest-palindromic-substring/solution.py
@@ -0,0 +1,48 @@
+class Solution(object):
+
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        max_len = 0
+        rslt = ''
+        str_len = len(s)
+        palindrom_str_len = 0
+        palindrom_str = ''
+
+        # if str_len == 1:
+        #   return s
+
+        for idx, c in enumerate(s):
+            if max_len == 0:
+                max_len = 1
+                rslt = c
+
+            if(idx >= 1) and (idx + 1 < str_len) and (s[idx - 1] == s[idx + 1]):
+                i = 1
+                while (idx >= i and idx + i < str_len and s[idx - i] == s[idx + i]):
+                    if 2 * i + 1 > max_len:
+                        max_len = 2 * i + 1
+                        rslt = s[idx - i:idx + i + 1]
+
+                    i += 1
+
+            if(idx + 1 < str_len) and (s[idx] == s[idx + 1]):
+                i = 0
+                while (idx >= i and idx + i + 1 < str_len and s[idx - i] == s[idx + 1 + i]):
+                    if 2 * (i+1) > max_len:
+                        max_len = 2 * (i + 1)
+                        rslt = s[idx - i:idx + 1 + i + 1]
+
+                    i += 1
+
+        return rslt
+
+foo = Solution()
+print(foo.longestPalindrome('b') == 'b')
+print(foo.longestPalindrome('bb') == 'bb')
+print(foo.longestPalindrome('abcabcba') == 'abcba')
+print(foo.longestPalindrome('abcdcbaabc') == 'abcdcba')
+print(foo.longestPalindrome("abababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababa") == "abababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababa")

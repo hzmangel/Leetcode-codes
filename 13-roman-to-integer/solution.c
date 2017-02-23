93ae6e474ba2e54c956589d9ed19f5213b718626
Hu Ziming <hzmangel@gmail.com>
2017-02-23 14:06:01 +0800

    AC, 72ms, 17.71%, 😭

--- 13-roman-to-integer/solution.c
+++ 13-roman-to-integer/solution.c
@@ -2,55 +2,28 @@
 #include <stdbool.h>
 
 int romanChToInt(char c) {
-    int result = 0;
-
     switch(c) {
-        case 'I':
-        case 'i':
-            result = 1;
-            break;
-        case 'V':
-        case 'v':
-            result = 5;
-            break;
-        case 'X':
-        case 'x':
-            result = 10;
-            break;
-        case 'L':
-        case 'l':
-            result = 50;
-            break;
-        case 'C':
-        case 'c':
-            result = 100;
-            break;
-        case 'D':
-        case 'd':
-            result = 500;
-            break;
-        case 'M':
-        case 'm':
-            result = 1000;
-            break;
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default:  return 0;
     }
-
-    return result;
 }
 
 int romanToInt(char* s) {
     char *p = s;
     int result = 0;
 
-    int prev_val = 32767;
-    int curr_val = -1;
+    int prev_val = 1000;
+    int curr_val = 0;
 
     while(*p != 0) {
         curr_val = romanChToInt(*p);
-        if (curr_val > prev_val) {
-            result -= prev_val * 2;
-        }
-        result += curr_val;
+        result += curr_val > prev_val ? (curr_val - (prev_val << 1)) : curr_val;
         prev_val = curr_val;
         p++;
     }

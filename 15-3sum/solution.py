b6a7ca2f9376f39a78b3d28031c06453bed23495
Hu Ziming <hzmangel@gmail.com>
2017-02-26 21:00:21 +0800

    AC, 1442ms, 8.21%, 😭
    Actually there was a lot of timeout errors before…

---
+++ 15-3sum/solution.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    @profile
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        nums.sort()
+        arr_len = len(nums)
+
+        rslt = []
+        for idx in range(arr_len - 2):
+            if idx > 0 and nums[idx] == nums[idx - 1]:
+                continue
+
+            target_val = 0 - num
+            l_idx, r_idx = idx + 1, arr_len - 1
+
+            while (l_idx < r_idx):
+                if l_idx == idx or r_idx == idx:
+                    break
+
+                if nums[l_idx] + nums[r_idx] == target_val:
+                    rslt.append(tuple(sorted([nums[l_idx], nums[r_idx], num])))
+                    curr_l = nums[l_idx]
+                    curr_r = nums[r_idx]
+
+                    while((l_idx < arr_len) and (nums[l_idx] == curr_l)):
+                        l_idx += 1
+
+                    while((r_idx > 0) and (nums[r_idx] == curr_r)):
+                        r_idx -= 1
+
+                elif nums[l_idx] + nums[r_idx] < target_val:
+                    l_idx += 1
+                elif nums[l_idx] + nums[r_idx] > target_val:
+                    r_idx -= 1
+
+        return rslt
+
+
+foo = Solution()
+print(foo.threeSum([0] * 1000))

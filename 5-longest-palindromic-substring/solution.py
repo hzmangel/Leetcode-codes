bfa89ea205d5b1f548ce1a02e0770d0f618dfd51
Hu Ziming <hzmangel@gmail.com>
2017-04-02 11:44:13 +0800

    AC, 625ms, 41.53%
    其实和之前那个代码的逻辑差不多，不知道为什么就快了一点...

--- 5-longest-palindromic-substring/solution.py
+++ 5-longest-palindromic-substring/solution.py
@@ -4,52 +4,43 @@ import timeit
 
 class Solution(object):
 
+    def findPalindrome(self, s, l_end_idx, r_start_idx):
+        offset = 0
+        max_len = r_start_idx - l_end_idx + 1
+        str_len = len(s)
+
+        while l_end_idx - offset >= 0 and r_start_idx + offset < str_len:
+            if s[l_end_idx - offset] == s[r_start_idx + offset]:
+                max_len += 2
+                offset += 1
+            else:
+                return max_len, s[l_end_idx-offset+1:r_start_idx+offset]
+
+        return max_len, s[l_end_idx-offset+1:r_start_idx+offset]
+
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
 
+        if s is None or len(s) == 1:
+            return s
+
+        str_len = len(s)
         max_len = 0
         rslt = ''
-        str_len = len(s)
 
         for idx, c in enumerate(s):
-            if max_len == 0:
-                max_len = 1
-                rslt = c
+            tmp_max_len, tmp_rslt = self.findPalindrome(s, idx-1, idx+1)
+            if tmp_max_len > max_len:
+                max_len = tmp_max_len
+                rslt = tmp_rslt
 
-            if(idx >= 1) and (idx + 1 < str_len) and (s[idx - 1] == s[idx + 1]):
-                i = 0
-
-                first_half = s[:idx][::-1]
-                second_half = s[idx + 1:]
-                limit = min(len(first_half), len(second_half))
-
-                for i in range(limit):
-                    if first_half[i] != second_half[i]:
-                        i -= 1
-                        break
-
-                i += 1
-                if 2 * i + 1 > max_len:
-                    max_len = 2 * i + 1
-                    rslt = s[idx - i:idx + i + 1]
-
-            if(idx + 1 < str_len) and (s[idx] == s[idx + 1]):
-                i = 0
-                first_half = s[:idx+1][::-1]
-                second_half = s[idx + 1:]
-                limit = min(len(first_half), len(second_half))
-
-                for i in range(limit):
-                    if first_half[i] != second_half[i]:
-                        i -= 1
-                        break
-
-                if 2 * (i + 1) > max_len:
-                    max_len = 2 * (i + 1)
-                    rslt = s[idx - i:idx + 1 + i + 1]
+            tmp_max_len, tmp_rslt = self.findPalindrome(s, idx, idx+1)
+            if tmp_max_len > max_len:
+                max_len = tmp_max_len
+                rslt = tmp_rslt
 
         return rslt
 

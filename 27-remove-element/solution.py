896f1b15ba15bc1d2b9173af14f92feafa2a02c3
Hu Ziming <hzmangel@gmail.com>
2017-05-15 00:31:06 +0800

    AC, 65ms, 15.65%
    尝试把2-pass改成1-pass，性能更差了，再想想看。。。

--- 27-remove-element/solution.py
+++ 27-remove-element/solution.py
@@ -12,36 +12,55 @@ class Solution(object):
         arr_length = len(nums)
         rslt = arr_length
 
-        for i in range(arr_length):
-            if nums[i] == val:
-                nums[i] = None
-                rslt -= 1
+        if arr_length == 1:
+            if nums[0] == val:
+                nums = []
+                return 0
+            else:
+                return 1
 
-        last_idx = arr_length - 1  # Last index of array element
-        for i in range(arr_length):
-            if nums[i] is None:
-                while nums[last_idx] is None and i  < last_idx:
-                    last_idx -= 1
+        r_idx = arr_length - 1  # Right index of array elements
+        for l_idx in range(arr_length):
+            if l_idx > r_idx:
+                # The backward point has already crossed with forward index,
+                # all the elements from forward index are the given value,
+                # break.
+                break
+            elif l_idx == r_idx:
+                if nums[l_idx] == val:
+                    rslt -= 1
 
-                if i >= last_idx:
-                    # The backward point has already crossed with forward index,
-                    # all the elements from forward index are None, break.
-                    break
-                else:
-                    # Backward index still has element, swap them
-                    nums[i] = nums[last_idx]
-                    last_idx -= 1
+                break
+            else:
+                if nums[l_idx] == val:
+                    rslt -= 1
+                    while nums[r_idx] == val and l_idx < r_idx:
+                        r_idx -= 1
+                        rslt -= 1
+
+                    if l_idx < r_idx:
+                        # Backward index still has element, swap them
+                        nums[l_idx], nums[r_idx] = nums[r_idx], nums[l_idx]
+                        r_idx -= 1
 
         return rslt
-
 
 
 foo = Solution()
 nums = [1]
 print(foo.removeElement(nums, 1), nums)
 
+nums = [1,2]
+print(foo.removeElement(nums, 1), nums)
+
+nums = [1,2]
+print(foo.removeElement(nums, 2), nums)
+
 nums = [3,2,2,3]
 print(foo.removeElement(nums, 1), nums)
+
+nums = [3,2,2,3]
+print(foo.removeElement(nums, 3), nums)
 
 nums = [3,2,3,3,2,2,3]
 print(foo.removeElement(nums, 2), nums)
